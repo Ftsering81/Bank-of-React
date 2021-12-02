006c8ea32742066e81cbc00b8d56ec4888b52930
Fnu Tsering <fnu.tsering46@myhunter.cuny.edu>
2021-12-02 02:36:34 -0500

    Implemented the Debit Component and addDebit()

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Home from './components/Home'
 import UserProfile from './components/UserProfile';
 import LogIn from './components/Login'
 import axios from 'axios' //library for API requests 
-// import './App.css'
+import Debit from './components/Debit'
+import './App.css'
 
 class App extends Component {
 
@@ -35,12 +36,10 @@ class App extends Component {
     
     //    Get the sum of the debits and the sum of the credits   //
     let debitSum = 0, creditSum = 0;
-    
     //For each debit object in array debits, add the value of the amount property to DebitSum
     debits.forEach(( debit ) => {
       debitSum+= debit.amount 
     });
-   
     //For each credit object in array credits, add the value of the amount property to CreditSum
     credits.forEach(( credit ) => {
       creditSum+= credit.amount
@@ -52,6 +51,7 @@ class App extends Component {
     //Update the state property accountBalance to the new calculated balance and debits and credits to the data from the API requests 
     this.setState({accountBalance, debits, credits});
   }
+  
 
 
   //this function is called in the Login componenent and it updates the userName property of the state in this component with new username
@@ -59,6 +59,39 @@ class App extends Component {
     const newUser = {...this.state.currentUser};
     newUser.userName = logInfo.userName;
     this.setState({currentUser: newUser})
+  }
+  // The event is when the user clicks "Add Debit" button. 
+  // The event object will contain the values of the Description input field and the Amount input field
+  addDebit = (event) => {
+
+    event.preventDefault(); //prevent default return behavior
+    //let randomNumID = (Math.random()).toString() 
+    let amount = parseFloat(event.target.amount.value) //parseFloat() method converts a string into a point number
+    let description = event.target.description.value
+
+    // Gets the current date and puts it in YYYY-MM-DD format
+    let today = new Date(); //date object representing current date
+    let year = today.getFullYear().toString()
+    let month = today.getMonth().toString() 
+    if (month.length !== 2) { month = '0' + month }
+    let day = today.getDate().toString()
+    if(day.length !== 2) { day = '0' + day}
+    let date = year + '-' + month + '-' + day; // YYYY-MM-DD format
+
+    let newDebit = {id: "",
+                    amount: amount,
+                    description: description,
+                    date: date
+    }
+
+    let debits = [...this.state.debits]; //create a copy of the debits array 
+    debits.push(newDebit); //add the newDebit object to the end of the debits array
+  
+    let accountBalance = (this.state.accountBalance - amount).toFixed(2) //amount is the new debit amount that was specified in the Amount input field
+    
+    //Update the debits and accountBalance properties of the state with the new debit array and new balance
+    this.setState({debits: debits, accountBalance: accountBalance})
+
   }
 
   render() { //render is called first time in the first instantiation of the component, then it is called again whenever the state updates
@@ -69,6 +102,7 @@ class App extends Component {
     const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
     const UserProfileComponent = () => (<UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}/>); 
     const LogInComponent = () => ( <LogIn user={this.state.currentUser} mockLogin={this.mockLogIn} />);
+    const DebitComponent = () => (<Debit addDebit = {this.addDebit} debits = {this.state.debits} accountBalance={this.state.accountBalance} />); 
     
     //so basically this render() renders the Home Component at URL "/"
     return (
@@ -78,6 +112,7 @@ class App extends Component {
           <Route exact path="/" render= {HomeComponent}/>  {/* Creates a route with url "/" at which the Home component is mounted/displayed*/}
           <Route exact path="/userProfile" render={UserProfileComponent}/>
           <Route exact path="/login" render={LogInComponent} />
+          <Route exact path="/debit" render={DebitComponent} />
         </div>
       </Router>
     );
