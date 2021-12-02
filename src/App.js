76eb4c76b6e26832e5a6e715bde430b320eb3b60
Fnu Tsering <fnu.tsering46@myhunter.cuny.edu>
2021-12-01 21:27:06 -0500

    Implemented componentDidMount() in which the API requests are  made to get debits and credits data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home'
 import UserProfile from './components/UserProfile';
 import LogIn from './components/Login'
+import axios from 'axios' //library for API requests 
 // import './App.css'
 
 class App extends Component {
@@ -12,13 +13,47 @@ class App extends Component {
     super();
 
     this.state = {
-      accountBalance: 14568.27,
+      accountBalance: 0,
       currentUser: {
-        userName: 'joe_shmo',
-        memberSince: '07/23/96'
-      }
+        userName: '',
+        memberSince: '11/11/2021'
+      },
+      debits: [],
+      credits: []
     }
   }
+
+  //Lifecycle method that is called when the component is rendered for the first time. API requests are made here to get initial debits and credits.
+  async componentDidMount() {
+
+    let debits = await axios.get("https://moj-api.herokuapp.com/debits") //makes a get request to this debits API endpoint 
+    let credits = await axios.get("https://moj-api.herokuapp.com/credits") //makes a get request to this credits API endpoint
+
+    //Get the data from the API response objects
+    debits = debits.data //data is an array of objects that each represents a debit with properties "id", "description",  "amount", "date"
+    credits = credits.data //data is an array of objects that each represents a credit properties "id", "description",  "amount", "date"
+    
+    //    Get the sum of the debits and the sum of the credits   //
+    let debitSum = 0, creditSum = 0;
+    
+    //For each debit object in array debits, add the value of the amount property to DebitSum
+    debits.forEach(( debit ) => {
+      debitSum+= debit.amount 
+    });
+   
+    //For each credit object in array credits, add the value of the amount property to CreditSum
+    credits.forEach(( credit ) => {
+      creditSum+= credit.amount
+    });
+
+    //Compute the account balance from the debit sum and credit sum
+    let accountBalance = (creditSum - debitSum).toFixed(2); //toFixed(2) ensures we will only have 2 decimal digits with rounding
+
+    //Update the state property accountBalance to the new calculated balance and debits and credits to the data from the API requests 
+    this.setState({accountBalance, debits, credits});
+  }
+
+
   //this function is called in the Login componenent and it updates the userName property of the state in this component with new username
   mockLogIn = (logInfo) => {
     const newUser = {...this.state.currentUser};
@@ -26,7 +61,7 @@ class App extends Component {
     this.setState({currentUser: newUser})
   }
 
-  render() { //render is called first time in the first instantiation of the component, then it is called again when the state updats if there is one
+  render() { //render is called first time in the first instantiation of the component, then it is called again whenever the state updates
      
     // The component prop in Route won't accept a pre-built component, but React Router has provided an alternative render method/prop that will. 
     // So basically the render prop accepts a function variable that returns a pre-built componet.
